545a67ee1aa5b0ec39a2ac2e09c663297ea01260
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-24 13:31:23 +0530

    Client Refresh Scope
    Accessing modified configs from server without server restart

--- src/main/java/com/boot/demo/FranceConfigController.java
+++ src/main/java/com/boot/demo/FranceConfigController.java
@@ -1,12 +1,14 @@
 package com.boot.demo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/frx")
+@RefreshScope
 public class FranceConfigController {
 
 	@Value("${demo.api.key}")
